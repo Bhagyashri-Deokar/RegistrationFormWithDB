8c0454b00216fde3aa7157e53ecf610d96859692
Bhagyashri vitthal deokar <deokarbhagyashri1999@gmail.com>
2023-12-02 12:49:05 +0530

    hibernate validation

--- src/main/java/com/microservice/user/controller/UserController.java
+++ src/main/java/com/microservice/user/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,28 +15,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.user.model.User;
+import com.microservice.user.responsebody.ApiResponse;
 import com.microservice.user.services.UserServices;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1")
 public class UserController {
 	@Autowired
 	UserServices userServices;
 
 	@PostMapping("/user")
-	public ResponseEntity<?> saveUserInformation(@Valid @RequestBody User user)
+	public ResponseEntity<?> saveUserInformation(@Valid @RequestBody User user, BindingResult bindingResult)
 	{
-		return ResponseEntity.status(HttpStatus.OK).body(userServices.createUser(user));
+		
+		ApiResponse<String> apiResponse = new ApiResponse<String>();
+		
+		System.out.println("Error="+bindingResult.getErrorCount());
+		if (bindingResult.hasErrors()) {
+			apiResponse.setStatus(400);
+			apiResponse.setMessage("Validation failed for the following fields:");
+			apiResponse.setData(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+
+			return ResponseEntity.badRequest().body(apiResponse);
+		}
+
+		try {
+			// Call the service to save the user information
+			User savedUser = userServices.createUser(user);
+
+			return ResponseEntity.status(HttpStatus.OK).body(savedUser);
+		} catch (Exception e) {
+			System.out.println("Exception="+e.getMessage());
+			// Handle specific exception, you can create your own custom exception classes
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(e);
+		}
 	}
+
 	@PostMapping("/user/{id}")
-	public ResponseEntity<?> updateUserInformation(@PathVariable int id,@RequestBody User user)
-	{
-		User user_=userServices.getAllUserById(id);
+	public ResponseEntity<?> updateUserInformation(@PathVariable int id, @RequestBody User user) {
+		User user_ = userServices.getAllUserById(id);
 		return ResponseEntity.status(HttpStatus.OK).body(userServices.createUser(user));
 	}
+
 	@GetMapping("/user")
-	public ResponseEntity<?> getAllUser()
-	{
+	public ResponseEntity<?> getAllUser() {
 		return ResponseEntity.status(HttpStatus.OK).body(userServices.getAllUsers());
 	}
 }
--- src/main/java/com/microservice/user/model/User.java
+++ src/main/java/com/microservice/user/model/User.java
@@ -1,6 +1,5 @@
 package com.microservice.user.model;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,48 +7,63 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 @Entity
-@Table(name="user_registration_service")
-public class User 
-{
-	
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "user_id")
-@Id
-int userId;
+@Table(name = "user_registration_service")
+public class User {
 
-@NotBlank(message = "User Name should not blank")
-@NotEmpty(message = "User Name should not empty")
-@Column(name = "user_name")
-String userName;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    @Id
+    private int userId;
 
-@Column(name = "user_email")
-String userEmail;
+    @NotBlank(message = "Name cannot be empty. Please provide a name.")
+    @NotBlank(message = "Name cannot be empty. Please provide a name.")
+	@NotNull(message = "Name cannot be empty. Please provide a name.")
+	@Size(min = 3, max = 100, message = "Name length should be between 3 and 100 characters")
+	@Column(name = "user_name")
+    private String userName;
 
-@Column(name = "user_detail")
-String userDetails;
+    @NotBlank(message = "Email cannot be empty")
+	@NotNull(message = "Email cannot be empty")
+	@NotBlank(message = "Email cannot be empty")
+	@Email(message = "Please provide a valid email address")
+	 @Column(name = "user_email",unique = true)
+    private String userEmail;
 
-@Column(name = "phone_number")
-private String phoneNumber;
+    @Size(max = 255, message = "User details should not exceed 255 characters")
+    @Column(name = "user_detail")
+    private String userDetails;
 
-@Column(name = "address")
-private String address;
+    @Size(max = 15, message = "Phone number should not exceed 10 characters")
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
-@Column(name = "age")
-private int age;
+    @Size(max = 255, message = "Address should not exceed 255 characters")
+    @Column(name = "address")
+    private String address;
 
-@Column(name = "gender")
-private String gender;
+    @Min(value = 0, message = "Age should not be less than 0")
+    @Column(name = "age")
+    private int age;
 
-@Column(name = "qualification")
-private String qualification;
+    @Size(max = 10, message = "Gender should not exceed 10 characters")
+    @Column(name = "gender")
+    private String gender;
 
-@Column(name = "position")
-private String position;
+    @Size(max = 50, message = "Qualification should not exceed 50 characters")
+    @Column(name = "qualification")
+    private String qualification;
+
+    @Size(max = 50, message = "Position should not exceed 50 characters")
+    @Column(name = "position")
+    private String position;
 }
